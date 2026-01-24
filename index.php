c270a5c0a0b13a8929970291952f05efb37f93b1
fredriclesomar <fredric.sjl@gmail.com>
2026-01-25 00:56:14 +0700

    Optimize GPL v3

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php require 'import.php';
+<?php
+/**
+ * © 2026 Fredric Lesomar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+require 'import.php';
 ?>
 <!DOCTYPE html>
 <html lang="id">
